3aa90ae7e69b8e3bcf27c7926c7b48ebddd6c4a9
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2023-05-04 17:37:29 +0700

    update

---
+++ get_boolean_from_netlist.py
@@ -0,0 +1,20 @@
+from anytree import Node, RenderTree
+
+udo = Node("Udo")
+marc = Node("Marc", parent=udo)
+lian = Node("Lian", parent=marc)
+dan = Node("Dan", parent=udo)
+jet = Node("Jet", parent=dan)
+jan = Node("Jan", parent=dan)
+joe = Node("Joe", parent=dan)
+
+print(udo)
+
+print(joe)
+
+
+for pre, fill, node in RenderTree(udo):
+    print("%s%s" % (pre, node.name))
+
+
+print(udo.children)
